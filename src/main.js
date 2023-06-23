021fd5fdd1384930899ee7b76d04a5f64dc82bbd
Christian Wolter <48556854+chrisWolter@users.noreply.github.com>
2023-06-23 16:58:31 +0200

    Init

---
+++ src/main.js
@@ -0,0 +1,12 @@
+/*import { Camera, Group, Scene } from "https://cdn.skypack.dev/three@0.132.2";
+
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/controls/OrbitControls.js?module";
+import { GLTFLoader } from "https://cdn.skypack.dev/three@0.132.2/examples/jsm/loaders/GLTFLoader.js?module";
+*/
+import {
+    Camera,
+    Group,
+    Scene,
+    } from 'three';
+    import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+    import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
